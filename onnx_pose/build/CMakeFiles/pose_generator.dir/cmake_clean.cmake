15d988095363de071dadb0b2a664ad5e26c55fd5
Berke Yılmaz <95642259+berke-yilmaz@users.noreply.github.com>
2024-06-02 19:05:17 +0300

    Add files via upload

---
+++ onnx_pose/build/CMakeFiles/pose_generator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_generator.dir/pose_generator.cpp.o"
+  "CMakeFiles/pose_generator.dir/pose_generator.cpp.o.d"
+  "pose_generator"
+  "pose_generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
